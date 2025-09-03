89a8faf2d5d2d11130d7c2b5cd6ad91988843df2
Sinmiloluwa <mofeoluwae@gmail.com>
2025-09-03 13:04:33 +0100

    modified drawer

--- lib/views/widgets/home_page/custom_drawer.dart
+++ lib/views/widgets/home_page/custom_drawer.dart
@@ -6,23 +6,45 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: Colors.black,
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(color: Colors.lightGreenAccent[400]),
-              child: Text('Drawer Header'),
+          SizedBox(
+            height: 120, // set your fixed height here
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: CircleAvatar(
+                  backgroundColor: Colors.greenAccent[400],
+                  child: Text(
+                    userName[0].toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {},
-            ),
-          ],
-        ),
-      );
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () {
+              // Handle settings tap
+            },
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
