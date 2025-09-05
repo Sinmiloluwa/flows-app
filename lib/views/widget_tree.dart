5ecd8b1eb5afc719f173d0dfc0d47f09886b0ed7
Sinmiloluwa <mofeoluwae@gmail.com>
2025-09-05 11:52:34 +0100

    fix bugs

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,4 +1,3 @@
-import 'package:flows/views/pages/ai_chat_page.dart';
 import 'package:flows/views/pages/library_page.dart';
 import 'package:flows/views/pages/search_page.dart';
 import 'package:flutter/material.dart';
